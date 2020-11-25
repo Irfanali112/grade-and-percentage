d5a8b7368419111721d08bb72d3d23cbdacad74b
irfan alki <ali.khoso1234@gmail.com>
2020-11-25 12:18:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function check(){
+
+let totalmarks = document.getElementById("tmarks");
+let obtainmarks =document.getElementById("obtainmarks");
+
+let per = ducument.getElementById("per")
+
+let percentage = (obtainmarks/totalmarks)*100 ;
+
+if(percentage >= 80 && percentage <=100){
+
+ducument.getElementById("result").innerhtml="A+";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
